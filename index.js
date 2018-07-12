01a8602c569314c299eba8e6509f7110388fabd3
jenrose24 <rosienifer@icloud.com>
2018-07-12 23:13:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+var names= ["Ada","Brendan", "Ali"]
 
+function printBadges(names){
+ for (var counter = 0; counter<3; counter++)
+ console.log("Welcome " + names[counter] + "! "+ "You are employee #" + (counter+1) + ".")
+ return names
+}
 
-
-
-
-
+var tails = 1
+var counter = 0
+function tailsNeverFails() {
+ tails=Math.floor(Math.random()*2);
+}
 
 function tailsNeverFails() {
  var counter = 0;
